d52d5c970be9e632ea29d29739cb4656b0b8142d
Aby Jose <abyjose@stanford.edu>
2024-05-23 16:27:30 -0700

    working pos and ori control

--- project_starter/panda/controller.cpp
+++ project_starter/panda/controller.cpp
@@ -11,6 +11,9 @@
 
 #include <iostream>
 #include <string>
+#include <deque>
+#include <vector>
+#include <Eigen/Dense>
 
 using namespace std;
 using namespace Eigen;
@@ -27,6 +30,25 @@ enum State {
 	POSTURE = 0, 
 	MOTION
 };
+
+VectorXd savitzky_golay_coefficients(int window_size, int poly_order) {
+    int half_window = (window_size - 1) / 2;
+    MatrixXd A(window_size, poly_order + 1);
+    VectorXd t(window_size);
+    for (int i = -half_window; i <= half_window; ++i) {
+        t(i + half_window) = i;
+    }
+    for (int i = 0; i < window_size; ++i) {
+        for (int j = 0; j <= poly_order; ++j) {
+            A(i, j) = pow(t(i), j);
+        }
+    }
+    MatrixXd ATA = A.transpose() * A;
+    VectorXd b = A.col(1); // Derivative corresponds to the first power term
+    VectorXd coeffs = ATA.ldlt().solve(b);
+    return coeffs;
+}
+
 
 int main() {
 	// Location of URDF files specifying world and robot information
@@ -92,7 +114,31 @@ int main() {
 	// create a loop timer
 	runloop = true;
 	double control_freq = 1000;
+	double data_freq = 120;       // Position data frequency in Hz
+	const int window_size = 5;   // Choose an odd number
+	const int poly_order = 2;
 	Sai2Common::LoopTimer timer(control_freq, 1e6);
+	VectorXd sg_coeffs = savitzky_golay_coefficients(window_size, poly_order);
+    deque<Vector3d> position_history;
+	deque<double> time_history;
+
+	long long int data_counter = 0;
+    int data_interval = static_cast<int>(control_freq / data_freq);
+
+	// Step 1: Calibration
+	Vector3d OS_X_0 = robot->position(control_link, control_point);
+	Matrix3d OS_R_0 = robot->rotation(control_link);
+
+	Matrix3d OS_R_OT;
+	OS_R_OT << -1, 0, 0,
+				0, 0, 1,
+				0, 1, 0;
+	OS_R_OT = AngleAxisd(M_PI, Vector3d (0,0,1)).toRotationMatrix() * OS_R_OT;
+	Vector3d OT_X_0 = redis_client.getEigen(RIGID_BODY_POS);
+	Vector4d OT_Q_0 = redis_client.getEigen(RIGID_BODY_ORI);
+	Quaterniond quaternion(OT_Q_0[3], OT_Q_0[0], OT_Q_0[1], OT_Q_0[2]);
+	Matrix3d OT_R_0 = quaternion.toRotationMatrix(); 	// Convert the quaternion to a rotation matrix
+
 
 	while (runloop) {
 		timer.waitForNextLoop();
@@ -102,6 +148,61 @@ int main() {
 		robot->setQ(redis_client.getEigen(JOINT_ANGLES_KEY));
 		robot->setDq(redis_client.getEigen(JOINT_VELOCITIES_KEY));
 		robot->updateModel();
+
+		Vector3d OS_Xd;
+		Matrix3d OS_Rd;
+
+
+		if (data_counter % data_interval == 0 ) {
+
+			// Step 2: relative position and orientation read 
+			Vector3d pos = redis_client.getEigen(RIGID_BODY_POS);
+			Vector3d OT_X = pos;
+
+			Vector4d quat = redis_client.getEigen(RIGID_BODY_ORI);
+			Quaterniond quatObj(quat[3], quat[0], quat[1], quat[2]);
+			Matrix3d OT_R = quatObj.toRotationMatrix();
+
+			Vector3d OT_X_rel = OT_X - OT_X_0;
+			Matrix3d OT_R_rel = OT_R.transpose() * OT_R_0;
+
+			// Step 3: tracking position and orientation in OpenSai Frame
+			Vector3d OS_X = OS_X_0 + OS_R_OT * OT_X_rel;
+			Matrix3d OS_R = OS_R_OT * OT_R_rel * OS_R_OT.transpose() * OS_R_0;
+
+			OS_Xd = OS_X;
+			OS_Rd = OS_R;
+
+			//debugging prints
+			cout << OS_X << endl;
+
+			// Add position and time to history
+			position_history.push_back(OS_X);
+			time_history.push_back(time);
+
+			// Ensure we have enough data points to compute the velocity
+			if (position_history.size() > 1) {
+				// Compute the finite difference for velocity
+				Vector3d pos_prev = position_history[position_history.size() - 2];
+				double time_prev = time_history[time_history.size() - 2];
+
+				double dt = time - time_prev;
+				
+				Vector3d velocity = (OS_X - pos_prev) / dt;
+
+				// Print the smoothed velocity
+				cout << "Time: " << time << " Velocity: " << velocity.transpose() << endl;
+
+			// Keep only the latest 2 positions and times
+			if (position_history.size() > 2) {
+				position_history.pop_front();
+				time_history.pop_front();
+			}
+        }
+
+		}
+
+        data_counter++;
 	
 		if (state == POSTURE) {
 			// update task model 
@@ -119,14 +220,16 @@ int main() {
 				ee_pos = robot->position(control_link, control_point);
 				ee_ori = robot->rotation(control_link);
 
-				pose_task->setGoalPosition(ee_pos - Vector3d(-0.1, -0.1, 0.1));
-				pose_task->setGoalOrientation(AngleAxisd(M_PI / 6, Vector3d::UnitX()).toRotationMatrix() * ee_ori);
+				// pose_task->setGoalPosition(ee_pos - Vector3d(-0.1, -0.1, 0.1));
+				// pose_task->setGoalOrientation(AngleAxisd(M_PI / 6, Vector3d::UnitX()).toRotationMatrix() * ee_ori);
 				gripper_task->setGoalPosition(Vector2d(0.02, -0.02));
 
 				state = MOTION;
 			}
 		} else if (state == MOTION) {
 			// update goal position and orientation
+			pose_task->setGoalPosition(OS_Xd);
+			pose_task->setGoalOrientation(OS_Rd);
 
 			// update task model
 			N_prec.setIdentity();
